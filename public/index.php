aab28220260274975167c17f64ef92ae462651f0
Mkbewe13 <jakub@j15.digital>
2022-12-28 23:52:24 +0100

    ADD: EmailService

--- public/index.php
+++ public/index.php
@@ -1,10 +1,20 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
+use Symfony\Component\Mime\Email;
 
+require_once __DIR__.'/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../');
+$dotenv->load();
 $loader = new \Twig\Loader\FilesystemLoader(__DIR__.'/../templates');
 $twig = new \Twig\Environment($loader);
 
 $test = new \TmeApp\Services\Xlsx\SpreadsheetService();
 $test->getXlsx();
+
+
+
+$email = (new Email())->from('tmeapp@example.com')->to('someone@example.com')->subject('Products sheet')->text('Products data xlsx file')->attachFromPath('demo.xlsx');
+$emailService = new \TmeApp\Services\Email\EmailService($email);
+$emailService->sendEmail();
+
 echo $twig->render('home.twig');
---
+++ src/Services/Email/EmailService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace TmeApp\Services\Email;
+
+use Symfony\Component\Mailer\Mailer;
+use Symfony\Component\Mailer\Transport;
+use Symfony\Component\Mime\Email;
+
+class EmailService
+{
+    private Email $emailMessage;
+    private Mailer $mailer;
+    private Transport\TransportInterface $transport;
+
+    public function __construct(Email $emailMessage)
+    {
+        $this->emailMessage = $emailMessage;
+        $this->transport = \Symfony\Component\Mailer\Transport::fromDsn($this->getDSN());
+        $this->mailer = new \Symfony\Component\Mailer\Mailer($this->transport);
+    }
+
+    private function getDSN(){
+        if(!isset($_ENV['MAILER_DSN']) || !$_ENV['MAILER_DSN']){
+            throw new \Exception('MAILER_DSN is missing. Check .env file');
+        }
+
+        return $_ENV['MAILER_DSN'];
+    }
+
+    public function sendEmail(){
+        $this->mailer->send($this->emailMessage);
+    }
+
+}
